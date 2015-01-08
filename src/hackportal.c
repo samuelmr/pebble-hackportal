50c63ef45d48555d99bd15a3cf6213ac95488d23
Samuel Rinnetmäki <samuel@iki.fi>
2015-01-08 10:24:49 +0200

    Lots of fixes and new features.
    Configuring new portals now hides old ones.
    
    Added burnout handling.
    
    Hack history is stored (but not permanently).
    
    Added menu icon.

--- src/hackportal.c
+++ src/hackportal.c
@@ -1,20 +1,22 @@
 #include <pebble.h>
   
-#define MAX_PORTAL_COUNT 3
+#define MAX_PORTAL_COUNT 20
 
 static Window *window;
-static const int MAX_HACKS = 16;
-static const uint32_t EPOCH = 1388527200;
+static const uint32_t EPOCH = 1388527200; // beginning of cycle 2014.01
+static const int EPOCH_YEAR = 2014;
 static const uint32_t SECS_IN_CYCLE = 630000;  
 static const uint32_t SECS_IN_CHECKPOINT = 18000;
+static const int16_t SIGNIFICANT_TIME = 4 * 60 * 60;
 static char time_text[] = "2014.43     29/35    4:53:23";
-int portal_count = 0;
+static int16_t portal_count = 0;
 
 enum MessageKey {
-  INDEX = 0x1,    // TUPLE_INT
-  NAME = 0x2,     // TUPLE_CSTRING
-  COOLDOWN = 0x3, // TUPLE_INT
-  HACKS = 0x4    // TUPLE_INT
+  INDEX = 0,    // TUPLE_INT
+  NAME = 1,     // TUPLE_CSTRING
+  COOLDOWN = 2, // TUPLE_INT
+  HACKS = 3,    // TUPLE_INT
+  PORTALS = 4   // TUPLE_INT
 };
 static MenuLayer *menu_layer;
 
@@ -36,15 +38,11 @@ Portal portals[MAX_PORTAL_COUNT];
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   time_t rt = time(NULL);
 	uint32_t t = rt - EPOCH;
-  uint32_t cycle = t / SECS_IN_CYCLE;
+  uint32_t cycle = (t / SECS_IN_CYCLE)%50;
 	uint32_t checkpoint = (t % SECS_IN_CYCLE) / SECS_IN_CHECKPOINT + 1;
 	uint32_t countdown = SECS_IN_CHECKPOINT - (t % SECS_IN_CHECKPOINT);
 	uint32_t next = rt + countdown;
-  int year = tick_time->tm_year + 1900;
-  int month = tick_time->tm_mon;
-  if ((cycle > 47) && (month < 2)) {
-    year--;
-  }
+  int year = EPOCH_YEAR + (int) ((t / SECS_IN_CYCLE) / 50);
   int hours = (int) (countdown / (60 * 60));
   int minutes = (countdown - (hours * 60 * 60)) / 60;
   int seconds = countdown - (hours * 60 * 60) - (minutes * 60);
@@ -52,10 +50,8 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   struct tm *tms;
   tms = localtime((time_t*) &next);
   strftime(next_str, sizeof(next_str), "%Y-%m-%d %H:%M:%S", tms);
-  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Time: %lu, (now: %lu), countdown %lu", t, rt, countdown);
-  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Coming up: %s", next_str);
-  snprintf(time_text, sizeof(time_text), "%d.%02lu     %02lu/35    %d:%02d:%02d", year, cycle, checkpoint, hours, minutes, seconds);  
-  // text_layer_set_text(text_time_layer, time_text);
+  snprintf(time_text, sizeof(time_text), "%d.%02lu     %02lu/35    %d:%02d:%02d",
+                                         year, cycle, checkpoint, hours, minutes, seconds);  
   layer_mark_dirty(menu_layer_get_layer(menu_layer));
 }
 
@@ -64,14 +60,17 @@ static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data
 }
 
 static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Menu rows: %d", portal_count);
   return portal_count;
 }
 
 static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Menu header height: %d", MENU_CELL_BASIC_HEADER_HEIGHT);
   return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Time: %s", time_text);
   menu_cell_basic_header_draw(ctx, cell_layer, time_text);
 }
 
@@ -88,33 +87,66 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
     }
     port->seconds--;  
   }
+  int shift = 0;
+  time_t now = time(NULL);  
+  for (int i=0; i<port->hacks_done; i++) {
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "Old hack %d: %d, %d (%d)", i, (int) port->hacked[i], port->cooldown_time, (int) now);
+    if ((int) (now - port->hacked[i]) > SIGNIFICANT_TIME) {
+      shift++;
+    }
+  }
+  if (shift > 0) {
+    for (int i=shift; i<port->hacks_done; i++) {
+      port->hacked[i-shift] = port->hacked[i];
+    }
+  }
+  port->hacks_done -= shift;
+  char pre_text[25];
+  if (port->hacks_done < port->hacks) {
+    snprintf(pre_text, sizeof(pre_text), "Hack %d/%d in", port->hacks_done + 1, port->hacks);
+  }
+  else {
+    strcpy(pre_text, "Burned out for");
+    port->seconds = SIGNIFICANT_TIME - (int) (now - port->hacked[0]);
+  }
   int hours = (int) port->seconds/3600;
   int minutes = (int) (port->seconds - (hours * 3600))/60;
   int seconds = port->seconds - (hours * 3600) - (minutes * 60);
-  char time_text[25];
-  snprintf(time_text, sizeof(time_text), "Hack %d/%d in %d:%02d:%02d", port->hacks_done + 1, port->hacks, hours, minutes, seconds);  
+  char time_text[27];
+  snprintf(time_text, sizeof(time_text), "%s %d:%02d:%02d", pre_text, hours, minutes, seconds);  
   menu_cell_basic_draw(ctx, cell_layer, port->name, time_text, NULL);
 }
 
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
   Portal *port = &portals[cell_index->row];
+  // hacks should be stored!
   port->hacked[port->hacks_done++] = time(NULL);
   port->seconds = port->cooldown_time;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Hacked portal %d: %s (%d/%d)", cell_index->row, port->name, port->hacks_done, port->hacks);
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Hacked portal %d: %s (%d/%d)", cell_index->row, port->name, port->hacks_done, port->hacks);
+  layer_mark_dirty(menu_layer_get_layer(menu_layer));
 }
 void in_received_handler(DictionaryIterator *received, void *context) {
-  // does not remove portals from list!
-  Tuple *it = dict_find(received, INDEX);
+  Tuple *it = dict_find(received, 0);
   int index = it->value->int8;
-  portal_count = (portal_count > index) ? portal_count : index;
-  Portal *port = &portals[index];
-  Tuple *nt = dict_find(received, NAME);
-  strcpy(port->name, nt->value->cstring);
-  Tuple *ct = dict_find(received, COOLDOWN);
-  port->cooldown_time = ct->value->int8;
-  Tuple *ht = dict_find(received, HACKS);
-  port->hacks = ht->value->int8;
-  port->hacks_done = 0;
+  if (index < MAX_PORTAL_COUNT) {
+    portal_count = (portal_count > index) ? portal_count : index;
+    Portal *port = &portals[index];
+    Tuple *nt = dict_find(received, NAME);
+    strcpy(port->name, nt->value->cstring);
+    Tuple *ct = dict_find(received, COOLDOWN);
+    port->cooldown_time = ct->value->uint32;
+    Tuple *ht = dict_find(received, HACKS);
+    port->hacks = ht->value->int8;
+    port->hacks_done = 0;
+    Tuple *lt = dict_find(received, PORTALS);
+    portal_count = lt->value->int8;
+    if (portal_count > MAX_PORTAL_COUNT) {
+      portal_count = MAX_PORTAL_COUNT;
+    }
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "Got configuration for portal %d: %s, %d, %d, %d", index, port->name, port->cooldown_time, port->hacks, port->hacks_done);    
+  }
+  menu_layer_reload_data(menu_layer);
+  layer_mark_dirty(menu_layer_get_layer(menu_layer));
 }
 void in_dropped_handler(AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Message from phone dropped: %d", reason);
@@ -129,8 +161,9 @@ void window_load(Window *window) {
     .get_header_height = menu_get_header_height_callback,
     .draw_header = menu_draw_header_callback,
     .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
+    .select_click = menu_select_callback
   });
+  
   menu_layer_set_click_config_onto_window(menu_layer, window);
   layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
   
@@ -141,26 +174,17 @@ void window_load(Window *window) {
   port1->hacks_done = 0;
   port1->seconds = 0;
   portal_count++;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Initialized portal 1: %s", port1->name);
-  Portal *port2 = &portals[1];
-  strcpy(port2->name, "Unnamed Portal 2");
-  port2->cooldown_time = 300;
-  port2->hacks = 4;
-  port2->hacks_done = 0;
-  port2->seconds = 0;
-  portal_count++;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Initialized portal 2: %s", port2->name);
-  Portal *port3 = &portals[2];
-  strcpy(port3->name, "Unnamed Portal 3");
-  port3->cooldown_time = 300;
-  port3->hacks = 4;
-  port3->hacks_done = 0;
-  port3->seconds = 0;
-  portal_count++;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Initialized portal 3: %s", port3->name); 
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Initialized portal 1: %s", port1->name);
+  menu_layer_reload_data(menu_layer);
 }
 
 static void window_unload(Window *window) {
+  // for (int i=0; i<MAX_PORTAL_COUNT; i++) {
+  //   if (portals[i]) {
+  //     free(portals[i]);
+  //   }
+  // }
+  // free(portals);
   menu_layer_destroy(menu_layer);
   // delete portals?
 }
@@ -173,13 +197,12 @@ static void init(void) {
   const uint32_t outbound_size = 128;
   app_message_open(inbound_size, outbound_size);
   window = window_create();
+  // window_set_fullscreen(window, true);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   window_stack_push(window, true);
-  app_event_loop();
-  window_destroy(window);
 }
 
 static void deinit(void) {
@@ -188,5 +211,6 @@ static void deinit(void) {
 
 int main(void) {
   init();
+  app_event_loop();
   deinit();
 }
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,19 +1,20 @@
-
 var initialized = false;
-
 var config = {};
 // var setPebbleToken = "YJHH";
+var messageQueue = [];
 
 Pebble.addEventListener("ready", function(e) {
   config = JSON.parse(localStorage.getItem("config"));
   initialized = true;
   console.log("JavaScript app ready and running! " + e.ready);
+  sendConfig(config);
 });
 
 Pebble.addEventListener("showConfiguration",
   function() {
     // var uri = "http://x.setpebble.com/" + setPebbleToken + "/" + Pebble.getAccountToken();
-    var uri = "https://rawgithub.com/samuelmr/pebble-hackportal/master/configure.html";
+    var uri = "https://rawgithub.com/samuelmr/pebble-hackportal/master/configure.html#" +
+              JSON.stringify(config);
     console.log("Configuration url: " + uri);
     Pebble.openURL(uri);
   }
@@ -25,22 +26,37 @@ Pebble.addEventListener("webviewclosed",
     console.log("Webview window returned: " + webconfig);
     localStorage.setItem("config", webconfig);
     config = JSON.parse(webconfig);
-    for (var i=0; i<config.length; i++) {
-      var msg = {"0": i, "1": config[i].name, "2": config[i].cooldown, "3": config[i].hacks};
-      sendMessage(msg);
-    }
+    sendConfig(config);
   }
 );
 
-function sendMessage(dict) {
-  Pebble.sendAppMessage(dict, appMessageAck, appMessageNack);
-  console.log("Sent message to Pebble! " + JSON.stringify(dict));
+function sendConfig(config) {
+  for (var i=0; i<config.length; i++) {
+    var msg = {"0": i,
+               "1": config[i].name,
+               "2": parseInt(config[i].cooldown),
+               "3": parseInt(config[i].hacks),
+               "4": config.length};
+    messageQueue.push(msg);
+  }
+  sendNextMessage();
+}
+
+function sendNextMessage() {
+  if (messageQueue.length > 0) {
+    Pebble.sendAppMessage(messageQueue[0], appMessageAck, appMessageNack);
+    console.log("Sent message to Pebble! " + JSON.stringify(messageQueue[0]));
+  }
 }
 
 function appMessageAck(e) {
   console.log("Message accepted by Pebble!");
+  messageQueue.shift();
+  sendNextMessage();
 }
 
 function appMessageNack(e) {
   console.log("Message rejected by Pebble! " + e.error);
+  // try again
+  sendNextMessage();
 }
