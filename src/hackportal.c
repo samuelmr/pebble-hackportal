1e13cb0fd94a951d095ba9de69b8c7174325ba7e
Samuel Rinnetmäki <samuel@iki.fi>
2015-05-13 20:07:17 +0300

    OK, use timer for changing seconds...

--- src/hackportal.c
+++ src/hackportal.c
@@ -39,33 +39,6 @@ typedef struct{
 
 Portal portals[MAX_PORTAL_COUNT];
 
-static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
-  time_t rt = time(NULL);
-  uint32_t t = rt - EPOCH;
-  uint32_t cycle = (t / SECS_IN_CYCLE)%50;
-  uint32_t checkpoint = (t % SECS_IN_CYCLE) / SECS_IN_CHECKPOINT + 1;
-  uint32_t countdown = SECS_IN_CHECKPOINT - (t % SECS_IN_CHECKPOINT);
-  uint32_t next = rt + countdown;
-  int year = EPOCH_YEAR + (int) ((t / SECS_IN_CYCLE) / 50);
-  int hours = (int) (countdown / (60 * 60));
-  int minutes = (countdown - (hours * 60 * 60)) / 60;
-  int seconds = countdown - (hours * 60 * 60) - (minutes * 60);
-  char next_str[80];
-  struct tm *tms;
-  tms = localtime((time_t*) &next);
-  strftime(next_str, sizeof(next_str), "%Y-%m-%d %H:%M:%S", tms);
-  snprintf(time_text, sizeof(time_text), "%d.%02lu     %02lu/35    %d:%02d:%02d",
-                                         year, cycle, checkpoint, hours, minutes, seconds);  
-  layer_mark_dirty(menu_layer_get_layer(menu_layer));
-}
-
-static void wakeup_handler(WakeupId id, int32_t row) {
-  MenuIndex index = MenuIndex(0, (int) row);
-  MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
-  menu_layer_set_selected_index(menu_layer, index, align, true);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Woken up by wakeup %lu (portal %lu)!", id, row);
-}
-
 void send_portal_hacks(int index, Portal *port) {
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -83,12 +56,92 @@ void send_portal_hacks(int index, Portal *port) {
   }
   dict_write_end(iter);
   app_message_outbox_send();
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent hacks for portal %d (%d) to phone!", index, port->hacks_done);
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Sent hacks for portal %d (%d) to phone!", index, port->hacks_done);
   if (port->wakeup_id && wakeup_query(port->wakeup_id, NULL)) {
     wakeup_cancel(port->wakeup_id);
   }
   port->wakeup_id = wakeup_schedule(time(NULL) + port->seconds - WAKEUP_BEFORE, (int32_t) index, true);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Set wakeup timer for portal %d (%lu).", index, port->wakeup_id);
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Set wakeup timer for portal %d (%ld).", index, port->wakeup_id);
+}
+
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
+  time_t rt = time(NULL);
+  uint32_t t = rt - EPOCH;
+  uint32_t cycle = (t / SECS_IN_CYCLE)%50;
+  uint32_t checkpoint = (t % SECS_IN_CYCLE) / SECS_IN_CHECKPOINT + 1;
+  uint32_t countdown = SECS_IN_CHECKPOINT - (t % SECS_IN_CHECKPOINT);
+  uint32_t next = rt + countdown;
+  int year = EPOCH_YEAR + (int) ((t / SECS_IN_CYCLE) / 50);
+  int hours = (int) (countdown / (60 * 60));
+  int minutes = (countdown - (hours * 60 * 60)) / 60;
+  int seconds = countdown - (hours * 60 * 60) - (minutes * 60);
+  char next_str[80];
+  struct tm *tms;
+  tms = localtime((time_t*) &next);
+  strftime(next_str, sizeof(next_str), "%Y-%m-%d %H:%M:%S", tms);
+  snprintf(time_text, sizeof(time_text), "%d.%02ld     %02ld/35    %d:%02d:%02d",
+                                         year, cycle, checkpoint, hours, minutes, seconds);  
+  time_t now = time(NULL);
+  for (int i=0; i<MAX_PORTAL_COUNT; i++) {
+    Portal *port = &portals[i];
+    if (port != NULL) {
+      if (!port->seconds) {
+        continue;
+      }
+      if (port->seconds < 0) {
+        port->seconds = 0;
+        if (port->wakeup_id) {
+          time_t left = 0;
+          wakeup_query(port->wakeup_id, &left);
+          if (left > 0) {
+            port->seconds = left + WAKEUP_BEFORE;
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "Found wakeup timer %d for portal %d, set seconds to %d", (int) port->wakeup_id, i, port->seconds);
+            port->wakeup_id = 0;  
+          }
+        }
+      }
+      if (port->seconds > 0) {
+        if (port->seconds == 1) {
+          vibes_long_pulse();
+          MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
+          MenuIndex cell_index = {0, i};
+          menu_layer_set_selected_index(menu_layer, cell_index, align, true);
+        }
+        else if (port->seconds <= 5) {
+          vibes_double_pulse();
+        }
+        port->seconds--;
+      }
+      if (!port->seconds && port->wakeup_id) {
+        if (wakeup_query(port->wakeup_id, NULL)) {
+          wakeup_cancel(port->wakeup_id);
+        }
+        port->wakeup_id = 0;
+      }
+      int shift = 0;
+      for (int i=0; i<port->hacks_done; i++) {
+        // APP_LOG(APP_LOG_LEVEL_DEBUG, "Old hack %d: %d, %d (%d)", i, (int) port->hacked[i], port->cooldown_time, (int) now);
+        if ((int) (now - port->hacked[i]) > SIGNIFICANT_TIME) {
+          shift++;
+        }
+      }
+      if (shift > 0) {
+        for (int i=shift; i<port->hacks_done; i++) {
+          port->hacked[i-shift] = port->hacked[i];
+        }
+        port->hacks_done -= shift;
+        send_portal_hacks(i, port);
+      }
+    }
+  }
+  layer_mark_dirty(menu_layer_get_layer(menu_layer));
+}
+
+static void wakeup_handler(WakeupId id, int32_t row) {
+  MenuIndex index = MenuIndex(0, (int) row);
+  MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
+  menu_layer_set_selected_index(menu_layer, index, align, true);
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Woken up by wakeup %ld (portal %ld)!", id, row);
 }
 
 void in_received_handler(DictionaryIterator *received, void *context) {
@@ -125,7 +178,7 @@ void in_received_handler(DictionaryIterator *received, void *context) {
 }
 
 void in_dropped_handler(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Message from phone dropped: %d", reason);
+  APP_LOG(APP_LOG_LEVEL_WARNING, "Message from phone dropped: %d", reason);
 }
 
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
@@ -150,58 +203,14 @@ static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, ui
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
   Portal *port = &portals[cell_index->row];
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Menu row for portal %d", cell_index->row);
-  if (!port->seconds || (port->seconds < 0)) {
-    if (port->wakeup_id) {
-      time_t left;
-      wakeup_query(port->wakeup_id, &left);
-      port->seconds = left + WAKEUP_BEFORE;
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "Found wakeup timer %lu for portal %d, set seconds to %d", port->wakeup_id, cell_index->row, port->seconds);
-      port->wakeup_id = 0;
-    }
-    else {
-      port->seconds = 0;
-    }
-  }
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Seconds for portal %d: %d", cell_index->row, port->seconds);
-  if (port->seconds > 0) {
-    if (port->seconds == 1) {
-      vibes_long_pulse();
-      MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
-      menu_layer_set_selected_index(menu_layer, *cell_index, align, true);
-    }
-    else if (port->seconds <= 5) {
-      vibes_double_pulse();
-    }
-    port->seconds--;  
-  }
-  if (!port->seconds && port->wakeup_id) {
-    if (wakeup_query(port->wakeup_id, NULL)) {
-      wakeup_cancel(port->wakeup_id);
-    }
-    port->wakeup_id = 0;
-  }
-  int shift = 0;
-  time_t now = time(NULL);
-  for (int i=0; i<port->hacks_done; i++) {
-    // APP_LOG(APP_LOG_LEVEL_DEBUG, "Old hack %d: %d, %d (%d)", i, (int) port->hacked[i], port->cooldown_time, (int) now);
-    if ((int) (now - port->hacked[i]) > SIGNIFICANT_TIME) {
-      shift++;
-    }
-  }
-  if (shift > 0) {
-    for (int i=shift; i<port->hacks_done; i++) {
-      port->hacked[i-shift] = port->hacked[i];
-    }
-  }
-  port->hacks_done -= shift;
-
   char pre_text[25];
   if (port->hacks_done < port->hacks) {
     snprintf(pre_text, sizeof(pre_text), "Hack %d/%d in", port->hacks_done + 1, port->hacks);
   }
   else {
     strcpy(pre_text, "Burned out for");
-    port->seconds = SIGNIFICANT_TIME - (int) (now - port->hacked[0]);
+    port->seconds = SIGNIFICANT_TIME - (int) (time(NULL) - port->hacked[0]);
   }
   int hours = (int) port->seconds/3600;
   int minutes = (int) (port->seconds - (hours * 3600))/60;
@@ -210,9 +219,6 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
   snprintf(time_text, sizeof(time_text), "%s %d:%02d:%02d", pre_text, hours, minutes, seconds);  
   menu_cell_basic_draw(ctx, cell_layer, port->name, time_text, NULL);
 
-  if (shift > 0) {
-    send_portal_hacks(cell_index->row, port);
-  }
 }
 
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
@@ -273,14 +279,7 @@ void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
-  // for (int i=0; i<MAX_PORTAL_COUNT; i++) {
-  //   if (portals[i]) {
-  //     free(portals[i]);
-  //   }
-  // }
-  // free(portals);
   menu_layer_destroy(menu_layer);
-  // delete portals?
 }
 
 static void init(void) {
@@ -288,11 +287,6 @@ static void init(void) {
   wakeup_service_subscribe(wakeup_handler);
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
-/*
-const uint32_t inbound_size = 256;
-  const uint32_t outbound_size = 256;
-  app_message_open(inbound_size, outbound_size);
-*/
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 
   window = window_create();
