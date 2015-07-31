1ec605c86354f7363c7d2eb0937a7abd2ae653b1
Samuel Rinnetmäki <samuel@iki.fi>
2015-07-31 11:52:17 +0300

    Settings for vibes and auto hide

--- src/hackportal.c
+++ src/hackportal.c
@@ -3,24 +3,32 @@
 #define MAX_PORTAL_COUNT 20
 #define MAX_HACKS 32
 #define WAKEUP_BEFORE 12
+#define OPTIONS_LENGTH 2
 
 static Window *window;
-static const uint32_t EPOCH = 1388527200; // beginning of cycle 2014.01
+static const uint32_t EPOCH = 1388520000; // beginning of cycle 2014.01
 static const int EPOCH_YEAR = 2014;
 static const uint32_t SECS_IN_CYCLE = 630000;  
 static const uint32_t SECS_IN_CHECKPOINT = 18000;
 static const int16_t SIGNIFICANT_TIME = 4 * 60 * 60;
 static char time_text[] = "2014.43     29/35    4:53:23";
 static int16_t portal_count = 0;
-static int silent;
 
-static char *silent_indicator;
-static char *mode[2];
-static int MODE_KEY = 6;
 enum MODES {
-  VIBE = 0,
-  NO_VIBE = 1
+  OFF = 0,
+  ON = 1
 };
+static char *mode[4];
+
+typedef struct{
+  int key;
+  int value;
+  char name[31];
+} Option;
+
+static Option options[OPTIONS_LENGTH];
+static Option *vibes;
+static Option *hide;
 
 enum MessageKey {
   PORTALS = 0,   // TUPLE_INT
@@ -46,13 +54,7 @@ typedef struct{
   WakeupId wakeup_id;
 } Portal;
 
-Portal portals[MAX_PORTAL_COUNT];
-
-static void tap_handler(AccelAxisType axis, int32_t direction) {
-  silent = !silent;
-  silent_indicator = mode[silent];
-  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Tapped, set silent to %d", (int) silent);
-}
+static Portal portals[MAX_PORTAL_COUNT];
 
 void send_portal_hacks(int index, Portal *port) {
   DictionaryIterator *iter;
@@ -78,10 +80,10 @@ void send_portal_hacks(int index, Portal *port) {
   if (port->seconds > WAKEUP_BEFORE) {
     port->wakeup_id = wakeup_schedule(time(NULL) + port->seconds - WAKEUP_BEFORE, (int32_t) index, true);
     if (port->wakeup_id > 0) {
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "Set wakeup timer for portal %d after %d seconds (%ld).", index, port->seconds - WAKEUP_BEFORE, port->wakeup_id);
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Set wakeup timer for portal %d after %d seconds (%ld).", index, port->seconds - WAKEUP_BEFORE, (long) port->wakeup_id);
     }
     else {
-      APP_LOG(APP_LOG_LEVEL_WARNING, "Could not set wakeup timer for portal %d after %d seconds: %ld.", index, port->seconds - WAKEUP_BEFORE, port->wakeup_id);
+      APP_LOG(APP_LOG_LEVEL_WARNING, "Could not set wakeup timer for portal %d after %d seconds: %ld.", index, port->seconds - WAKEUP_BEFORE, (long) port->wakeup_id);
     }
   }
 }
@@ -126,8 +128,8 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   struct tm *tms;
   tms = localtime((time_t*) &next);
   strftime(next_str, sizeof(next_str), "%Y-%m-%d %H:%M:%S", tms);
-  snprintf(time_text, sizeof(time_text), "%d.%02ld  %02ld/35  %s  %d:%02d:%02d",
-    year, cycle, checkpoint, silent_indicator, hours, minutes, seconds);  
+  snprintf(time_text, sizeof(time_text), "%d.%02ld   %02ld/35    %d:%02d:%02d",
+    year, (long) cycle, (long) checkpoint, hours, minutes, seconds);  
   for (int i=0; i<MAX_PORTAL_COUNT; i++) {
     Portal *port = &portals[i];
     if (port != NULL) {
@@ -140,7 +142,7 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
       }
       if (port->seconds > 0) {
         if (port->seconds == 1) {
-          if (!silent) {
+          if (vibes->value) {
             vibes_long_pulse();
           }
           MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
@@ -148,7 +150,7 @@ static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
           menu_layer_set_selected_index(menu_layer, cell_index, align, true);
         }
         else if (port->seconds <= 5) {
-          if (!silent) {
+          if (vibes->value) {
             vibes_double_pulse();
           }
         }
@@ -174,20 +176,22 @@ static void wakeup_handler(WakeupId id, int32_t row) {
   MenuIndex index = MenuIndex(0, (int) row);
   MenuRowAlign align = (portal_count <= 3) ? MenuRowAlignNone : MenuRowAlignCenter;
   menu_layer_set_selected_index(menu_layer, index, align, true);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Woken up by wakeup %ld (portal %ld)!", id, row);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Woken up by wakeup %ld (portal %ld)!", (long) id, (long) row);
   Portal *port = &portals[row];
   if (port->wakeup_id) {
     time_t left = 0;
     wakeup_query(port->wakeup_id, &left);
     if (left > 0) {
       port->seconds = left + WAKEUP_BEFORE;
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "Found wakeup timer %d for portal %ld, set seconds to %d", (int) port->wakeup_id, row, port->seconds);
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Found wakeup timer %d for portal %ld, set seconds to %d", (int) port->wakeup_id, (long) row, port->seconds);
       port->wakeup_id = 0;  
     }
   }
   clear_old_hacks(row, port);
 }
-
+static void hide_all(void *data) {
+  window_stack_pop(window);
+}
 void in_received_handler(DictionaryIterator *received, void *context) {
   Tuple *lt = dict_find(received, PORTALS);
   portal_count = (lt->value->int8 < MAX_PORTAL_COUNT) ? lt->value->int8 : MAX_PORTAL_COUNT;
@@ -234,11 +238,14 @@ void in_dropped_handler(AppMessageResult reason, void *context) {
 }
 
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
-  return 1;
+  return 2;
 }
 
 static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "Menu rows: %d", portal_count);
+  if (section_index > 0) {
+    return OPTIONS_LENGTH;
+  }
   return portal_count;
 }
 
@@ -249,10 +256,19 @@ static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t s
 
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "Time: %s", time_text);
+  if (section_index > 0) {
+    menu_cell_basic_header_draw(ctx, cell_layer, "Options");
+    return;
+  }
   menu_cell_basic_header_draw(ctx, cell_layer, time_text);
 }
 
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  if (cell_index->section > 0) {
+    Option *opt = &options[cell_index->row];
+    menu_cell_basic_draw(ctx, cell_layer, opt->name, mode[opt->value], NULL);
+    return;
+  }
   Portal *port = &portals[cell_index->row];
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "Menu row for portal %d", cell_index->row);
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "Seconds for portal %d: %d", cell_index->row, port->seconds);
@@ -269,10 +285,14 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
   char time_text[27];
   snprintf(time_text, sizeof(time_text), "%s %d:%02d:%02d", pre_text, hours, minutes, seconds);  
   menu_cell_basic_draw(ctx, cell_layer, port->name, time_text, NULL);
-
 }
 
 void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  if (cell_index->section > 0) {
+    options[cell_index->row].value = !options[cell_index->row].value;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Toggled %s to %s", options[cell_index->row].name, mode[options[cell_index->row].value]);
+    return;
+  }
   Portal *port = &portals[cell_index->row];
   port->hacked[port->hacks_done++] = time(NULL);
   if (port->hacks_done < port->hacks) {
@@ -284,6 +304,13 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
   // APP_LOG(APP_LOG_LEVEL_DEBUG, "Hacked portal %d: %s (%d/%d)", cell_index->row, port->name, port->hacks_done, port->hacks);
   layer_mark_dirty(menu_layer_get_layer(menu_layer));
   send_portal_hacks(cell_index->row, port);
+  if (hide->value) {
+    app_timer_register(3000, hide_all, NULL);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Scheduled auto hide: %d", hide->value);  
+  }
+  else {    
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "No auto hide: %d", hide->value);  
+  }
 }
 
 void menu_long_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
@@ -335,18 +362,24 @@ static void window_unload(Window *window) {
 
 static void init(void) {
 
-  mode[VIBE] = " ¦";
-  mode[NO_VIBE] = "x";
-  silent = persist_exists(MODE_KEY) ? persist_read_int(MODE_KEY) : 0;
-  silent_indicator = mode[silent];
+  mode[ON] = "On";
+  mode[OFF] = "Off";
 
+  vibes = &options[0];
+  vibes->key = 1;
+  strcpy(vibes->name, "Vibrations");
+  vibes->value = persist_exists(vibes->key) ? persist_read_int(vibes->key) : ON;
+  
+  hide = &options[1];
+  hide->key = 2;
+  strcpy(hide->name, "Auto hide");
+  hide->value = persist_exists(hide->key) ? persist_read_int(hide->key) : OFF;
+  
   tick_timer_service_subscribe(SECOND_UNIT, &handle_tick);
   wakeup_service_subscribe(wakeup_handler);
   app_message_register_inbox_received(in_received_handler);
   app_message_register_inbox_dropped(in_dropped_handler);
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
-  accel_tap_service_subscribe(tap_handler);
 
   window = window_create();
   // window_set_fullscreen(window, true);
@@ -365,8 +398,9 @@ static void init(void) {
 
 static void deinit(void) {
   window_destroy(window);
-  accel_tap_service_unsubscribe();
-  persist_write_int(MODE_KEY, silent);
+  for (int i=0; i<OPTIONS_LENGTH; i++) {
+    persist_write_int(options[i].key, options[i].value);
+  }
 }
 
 int main(void) {
